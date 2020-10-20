b13217b28039b4ce9ad38e4888b549cce692d67b
Theo Weeden <theo.weeden@gmail.com>
2020-10-20 10:20:39 +0100

    Built an implementation for calculating thhe value of pi using Monte Carlo methods

---
+++ MonteCarloPi/MonteCarloPi/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let CalcPi iterations = 
+    let r = System.Random()
+    
+    let mutable total = 0
+    
+    for i in 1..iterations do
+        let x = r.NextDouble();
+        let y = r.NextDouble();
+        if Math.Pow(x, float 2) + Math.Pow(y, float 2) < float 1 then 
+            total <- total + 1
+
+    let probability = float total / float iterations 
+    let pi = probability * float 4
+    pi
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine(CalcPi 10000000)
+    0
